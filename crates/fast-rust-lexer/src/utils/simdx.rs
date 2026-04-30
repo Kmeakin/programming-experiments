910e28bf8384795bf5ab846279e9bc41747d5200
Karl Meakin <karlwfmeakin@gmail.com>
2026-04-30 21:27:54 +0100

    cleanup `simdx`

--- crates/fast-rust-lexer/src/utils/simdx.rs
+++ crates/fast-rust-lexer/src/utils/simdx.rs
@@ -13,12 +13,12 @@ const POWERS_OF_2: Simd<u8, 16> = Simd::from_array([
 
 #[allow(improper_ctypes)]
 unsafe extern "C" {
-        #[link_name = "llvm.aarch64.neon.vsri.v16i8"]
-        fn vsriq_n_u8(a: uint8x16_t, b: uint8x16_t, c: u32) -> uint8x16_t;
+    #[link_name = "llvm.aarch64.neon.vsri.v16i8"]
+    fn vsriq_n_u8(a: uint8x16_t, b: uint8x16_t, c: u32) -> uint8x16_t;
 
-        #[link_name = "llvm.aarch64.neon.addp.v16i8"]
-        fn vpaddq_u8(a: uint8x16_t, b: uint8x16_t) -> uint8x16_t;
-    }
+    #[link_name = "llvm.aarch64.neon.addp.v16i8"]
+    fn vpaddq_u8(a: uint8x16_t, b: uint8x16_t) -> uint8x16_t;
+}
 
 #[inline]
 fn movemask8(mask: Mask<i8, 8>) -> u64 { mask.to_bitmask() }
@@ -51,6 +51,23 @@ fn movemask32(mask: &Mask<i8, 32>) -> u64 {
 }
 
 #[inline]
+pub fn movemask64(mask: &Mask<i8, 64>) -> u64 {
+    unsafe {
+        let tuple = vld4q_u8(ptr::from_ref(mask).cast());
+        let v0 = tuple.0;
+        let v1 = tuple.1;
+        let v2 = tuple.2;
+        let v3 = tuple.3;
+        let t0 = vsriq_n_u8(v1, v0, 1);
+        let t1 = vsriq_n_u8(v3, v2, 1);
+        let t2 = vsriq_n_u8(t1, t0, 2);
+        let t3 = vsriq_n_u8(t2, t2, 4);
+        let t4 = vshrn_n_u16(vreinterpretq_u16_u8(t3), 4);
+        vget_lane_u64(vreinterpret_u64_u8(t4), 0)
+    }
+}
+
+#[inline]
 pub fn movemask<const N: usize>(mask: Mask<i8, N>) -> u64 {
     match N {
         8 => movemask8(mask.resize::<8>(false)),
@@ -58,44 +75,6 @@ pub fn movemask<const N: usize>(mask: Mask<i8, N>) -> u64 {
         32 => movemask32(&mask.resize::<32>(false)),
         64 => movemask64(&mask.resize::<64>(false)),
         _ => panic!("Unsupported vector length"),
-    }
-}
-
-#[inline]
-pub fn movemask64(mask: &Mask<i8, 64>) -> u64 {
-    unsafe {
-        let tuple = { vld4q_u8(ptr::from_ref(mask).cast()) };
-
-        let v0 = tuple.0; // ḾḾḾḾḾḾḾḾ ḬḬḬḬḬḬḬḬ ḚḚḚḚḚḚḚḚ ḀḀḀḀḀḀḀḀ ḿḿḿḿḿḿḿḿ ḭḭḭḭḭḭḭḭ ḛḛḛḛḛḛḛḛ ḁḁḁḁḁḁḁḁ MMMMMMMM IIIIIIII EEEEEEEE AAAAAAAA mmmmmmmm iiiiiiii eeeeeeee aaaaaaaa
-        let v1 = tuple.1; // ṈṈṈṈṈṈṈṈ ĴĴĴĴĴĴĴĴ ḞḞḞḞḞḞḞḞ ḄḄḄḄḄḄḄḄ ṉṉṉṉṉṉṉṉ ĵĵĵĵĵĵĵĵ ḟḟḟḟḟḟḟḟ ḅḅḅḅḅḅḅḅ NNNNNNNN JJJJJJJJ FFFFFFFF BBBBBBBB nnnnnnnn jjjjjjjj ffffffff bbbbbbbb
-        let v2 = tuple.2; // ṎṎṎṎṎṎṎṎ ḴḴḴḴḴḴḴḴ ḠḠḠḠḠḠḠḠ ḈḈḈḈḈḈḈḈ ṏṏṏṏṏṏṏṏ ḵḵḵḵḵḵḵḵ ḡḡḡḡḡḡḡḡ ḉḉḉḉḉḉḉḉ OOOOOOOO KKKKKKKK GGGGGGGG CCCCCCCC oooooooo kkkkkkkk gggggggg cccccccc
-        let v3 = tuple.3; // ṔṔṔṔṔṔṔṔ ḺḺḺḺḺḺḺḺ ḪḪḪḪḪḪḪḪ ḎḎḎḎḎḎḎḎ ṕṕṕṕṕṕṕṕ ḻḻḻḻḻḻḻḻ ḫḫḫḫḫḫḫḫ ḏḏḏḏḏḏḏḏ PPPPPPPP LLLLLLLL HHHHHHHH DDDDDDDD pppppppp llllllll hhhhhhhh dddddddd
-
-        //   ṈṈṈṈṈṈṈṈ ĴĴĴĴĴĴĴĴ ḞḞḞḞḞḞḞḞ ḄḄḄḄḄḄḄḄ ṉṉṉṉṉṉṉṉ ĵĵĵĵĵĵĵĵ ḟḟḟḟḟḟḟḟ ḅḅḅḅḅḅḅḅ NNNNNNNN JJJJJJJJ FFFFFFFF BBBBBBBB nnnnnnnn jjjjjjjj ffffffff bbbbbbbb
-        // | ḾḾḾḾḾḾḾḾ ḬḬḬḬḬḬḬḬ ḚḚḚḚḚḚḚḚ ḀḀḀḀḀḀḀḀ ḿḿḿḿḿḿḿḿ ḭḭḭḭḭḭḭḭ ḛḛḛḛḛḛḛḛ ḁḁḁḁḁḁḁḁ MMMMMMMM IIIIIIII EEEEEEEE AAAAAAAA mmmmmmmm iiiiiiii eeeeeeee aaaaaaaa >> 1
-        // = ṈḾḾḾḾḾḾḾ ĴḬḬḬḬḬḬḬ ḞḚḚḚḚḚḚḚ ḄḀḀḀḀḀḀḀ ṉḿḿḿḿḿḿḿ ĵḭḭḭḭḭḭḭ ḟḛḛḛḛḛḛḛ ḅḁḁḁḁḁḁḁ NMMMMMMM JIIIIIII FEEEEEEE BAAAAAAA nmmmmmmm jiiiiiii feeeeeee baaaaaaa
-        let t0 = vsriq_n_u8(v1, v0, 1);
-
-        //   ṔṔṔṔṔṔṔṔ ḺḺḺḺḺḺḺḺ ḪḪḪḪḪḪḪḪ ḎḎḎḎḎḎḎḎ ṕṕṕṕṕṕṕṕ ḻḻḻḻḻḻḻḻ ḫḫḫḫḫḫḫḫ ḏḏḏḏḏḏḏḏ PPPPPPPP LLLLLLLL HHHHHHHH DDDDDDDD pppppppp llllllll hhhhhhhh dddddddd
-        // | ṎṎṎṎṎṎṎṎ ḴḴḴḴḴḴḴḴ ḠḠḠḠḠḠḠḠ ḈḈḈḈḈḈḈḈ ṏṏṏṏṏṏṏṏ ḵḵḵḵḵḵḵḵ ḡḡḡḡḡḡḡḡ ḉḉḉḉḉḉḉḉ OOOOOOOO KKKKKKKK GGGGGGGG CCCCCCCC oooooooo kkkkkkkk gggggggg cccccccc >> 1
-        // = ṔṎṎṎṎṎṎṎ ḺḴḴḴḴḴḴḴ ḪḠḠḠḠḠḠḠ ḎḈḈḈḈḈḈḈ ṕṏṏṏṏṏṏṏ ḻḵḵḵḵḵḵḵ ḫḡḡḡḡḡḡḡ ḏḉḉḉḉḉḉḉ POOOOOOO LKKKKKKK HGGGGGGG DCCCCCCC pooooooo lkkkkkkk hggggggg dccccccc
-        let t1 = vsriq_n_u8(v3, v2, 1);
-
-        //   ṔṎṎṎṎṎṎṎ ḺḴḴḴḴḴḴḴ ḪḠḠḠḠḠḠḠ ḎḈḈḈḈḈḈḈ ṕṏṏṏṏṏṏṏ ḻḵḵḵḵḵḵḵ ḫḡḡḡḡḡḡḡ ḏḉḉḉḉḉḉḉ POOOOOOO LKKKKKKK HGGGGGGG DCCCCCCC pooooooo lkkkkkkk hggggggg dccccccc
-        //   ṈḾḾḾḾḾḾḾ ĴḬḬḬḬḬḬḬ ḞḚḚḚḚḚḚḚ ḄḀḀḀḀḀḀḀ ṉḿḿḿḿḿḿḿ ĵḭḭḭḭḭḭḭ ḟḛḛḛḛḛḛḛ ḅḁḁḁḁḁḁḁ NMMMMMMM JIIIIIII FEEEEEEE BAAAAAAA nmmmmmmm jiiiiiii feeeeeee baaaaaaa >> 2
-        // = ṔṎṈḾḾḾḾḾ ḺḴĴḬḬḬḬḬ ḪḠḞḚḚḚḚḚ ḎḈḄḀḀḀḀḀ ṕṏṉḿḿḿḿḿ ḻḵĵḭḭḭḭḭ ḫḡḟḛḛḛḛḛ ḏḉḅḁḁḁḁḁ PONMMMMM LKJIIIII HGFEEEEE DCBAAAAA ponmmmmm lkjiiiii hgfeeeee dcbaaaaa
-        let t2 = vsriq_n_u8(t1, t0, 2);
-
-        //   ṔṎṈḾḾḾḾḾ ḺḴĴḬḬḬḬḬ ḪḠḞḚḚḚḚḚ ḎḈḄḀḀḀḀḀ ṕṏṉḿḿḿḿḿ ḻḵĵḭḭḭḭḭ ḫḡḟḛḛḛḛḛ ḏḉḅḁḁḁḁḁ PONMMMMM LKJIIIII HGFEEEEE DCBAAAAA ponmmmmm lkjiiiii hgfeeeee dcbaaaaa
-        // | ṔṎṈḾḾḾḾḾ ḺḴĴḬḬḬḬḬ ḪḠḞḚḚḚḚḚ ḎḈḄḀḀḀḀḀ ṕṏṉḿḿḿḿḿ ḻḵĵḭḭḭḭḭ ḫḡḟḛḛḛḛḛ ḏḉḅḁḁḁḁḁ PONMMMMM LKJIIIII HGFEEEEE DCBAAAAA ponmmmmm lkjiiiii hgfeeeee dcbaaaaa >> 4
-        // = ṔṎṈḾṔṎṈḾ ḺḴĴḬḺḴĴḬ ḪḠḞḚḪḠḞḚ ḎḈḄḀḎḈḄḀ ṕṏṉḿṕṏṉḿ ḻḵĵḭḻḵĵḭ ḫḡḟḛḫḡḟḛ ḏḉḅḁḏḉḅḁ PONMPONM LKJILKJI HGFEHGFE DCBADCBA ponmponm lkjilkji hgfehgfe dcbadcba
-        let t3 = vsriq_n_u8(t2, t2, 4);
-
-        //   trunc(ṔṎṈḾṔṎṈḾḺḴĴḬḺḴĴḬ ḪḠḞḚḪḠḞḚḎḈḄḀḎḈḄḀ ṕṏṉḿṕṏṉḿḻḵĵḭḻḵĵḭ ḫḡḟḛḫḡḟḛḏḉḅḁḏḉḅḁ PONMPONMLKJILKJI HGFEHGFEDCBADCBA ponmponmlkjilkji hgfehgfedcbadcba >> 4)
-        // = ṔṎṈḾḺḴĴḬ ḪḠḞḚḎḈḄḀ ṕṏṉḿḻḵĵḭ ḫḡḟḛḏḉḅḁ PONMLKJI HGFEDCBA ponmlkji hgfedcba
-        let t4 = vshrn_n_u16(vreinterpretq_u16_u8(t3), 4);
-
-        vget_lane_u64(vreinterpret_u64_u8(t4), 0)
     }
 }
 
