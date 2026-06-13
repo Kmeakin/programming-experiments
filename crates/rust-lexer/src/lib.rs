8dfa91d4cd319cc499e985ec13c3cc4c81b8b7aa
Karl Meakin <karlwfmeakin@gmail.com>
2026-06-13 23:24:20 +0100

    rust-lexer: move helper functions to utils.rs

--- crates/rust-lexer/src/lib.rs
+++ crates/rust-lexer/src/lib.rs
@@ -4,7 +4,12 @@
     loop_match,
     explicit_tail_calls
 )]
-#![allow(incomplete_features)]
+#![allow(
+    clippy::missing_safety_doc,
+    clippy::wildcard_imports,
+    incomplete_features,
+    unsafe_op_in_unsafe_fn
+)]
 
 pub mod common;
 pub mod lexers;
